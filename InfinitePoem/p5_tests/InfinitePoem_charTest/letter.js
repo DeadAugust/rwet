fb4fafff860ac3bf08fad93e122108e6ea402757
DeadAugust <augustluhrs@gmail.com>
2019-05-01 14:55:56 -0400

    transfer from p5 editor, getting portrait movement down

---
+++ InfinitePoem/p5_tests/InfinitePoem_charTest/letter.js
@@ -0,0 +1,75 @@
+let defSize = 8;
+let defCol = "black";
+let defSpeed = 1;
+
+
+class Letter {
+  constructor(parameters) {
+    // constructor(c, s, col, spd, x, y, hX, hY){
+    this.char = parameters.c;
+    this.size = parameters.s || defSize;
+    this.grey = parameters.grey || defCol;
+    this.col = parameters.col || defCol;
+    this.speed = parameters.spd || defSpeed;
+    this.x = int(parameters.x || random(1, width));
+    this.y = int(parameters.y || random(1, height));
+    this.homeX = parameters.hX;
+    this.homeY = parameters.hY;
+    this.farX = abs(this.homeX - this.x);
+    this.farY = abs(this.homeY - this.y);
+    this.step = 50; //100 moves to home?
+    this.clock = this.step; //to count down
+    //this.poem  //from python JSON
+  }
+  move() {
+
+
+  }
+  display() {
+    fill(this.grey);
+    // fill(this.col);
+    textSize(this.size);
+    text(this.char, this.x, this.y);
+  }
+  goHome() {
+    //flicker issues (esp. with 0)
+    /*
+    if (this.x > this.homeX) {
+      this.x = int(this.x - (this.farX / this.step));
+    } else if (this.x < this.homeX) {
+      this.x = int(this.x + (this.farX / this.step));
+    }
+    if (this.y > this.homeY) {
+      this.y = int(this.y - (this.farY / this.step));
+    } else if (this.y < this.homeY) {
+      this.y = int(this.y + (this.farY / this.step));
+    }
+    */
+
+    //slightly correct, but way small
+    /*
+    let goX;
+    let goY;
+    if (this.x > this.homeX) {
+      goX = (this.x - this.homeX) / 2;
+    } else if (this.x < this.homeX) {
+      goX = (this.homeX - this.x) / 2;
+    } else {
+      goX = this.x;
+    }
+    if (this.y > this.homeY) {
+      goY = (this.y - this.homeY) / 2;
+    } else if (this.y < this.homeY) {
+      goY = (this.homeY - this.y) / 2;
+    } else {
+      goY = this.y;
+    }
+    this.x = goX;
+    this.y = goY;
+    */
+  }
+  interact() {
+
+  }
+
+}
---
+++ InfinitePoem/p5_tests/InfinitePoem_charTest/sketch.js
@@ -0,0 +1,110 @@
+let img; //100 x 136
+
+let greyArray = [];
+let greyIndex = 0;
+let sz = 8;
+let geneArray = [];
+let geneIndex = 0;
+
+let once = true;
+
+let obit; //1772 char (not well cleaned) 13600 needed, so repeated
+// let obitArray = [];
+
+function preload(){
+  marg = loadImage('margaretObit_grey.jpg');
+  obit = loadStrings('margaretObit.txt');
+}
+
+
+function setup() {
+  createCanvas(600, 816);
+  // frameRate(2);
+  marg.loadPixels();
+
+  obit = obit[0].split(" ");
+  obit = join(obit, "");
+  textAlign(CENTER);
+  textSize(8);
+
+  //- - - - - - - - greyscale portrait
+
+  //setting up array
+  let o = -1;
+  for (let x = 0; x < marg.width; x++){
+    for (let y = 0; y <marg.height; y++){
+      let index = ((y * marg.width) + x) * 4;
+      let g = marg.pixels[index+2]; //greyscale, but y tho
+      if (o < obit.length - 1){
+        o++;
+      } else {
+        o = 0;
+      }
+      let char = obit[o]; //letter from obitArray
+
+      let parameters = {
+        c: char ,
+        grey: g,
+        hX: (width / marg.width) * (x + 1),
+        hY: (height / marg.height) * (y + 1)
+      }
+
+      let letter = new Letter(parameters);
+      geneArray.push(letter);
+
+      greyIndex++;
+    }
+  }
+
+
+  // for (let i = 0; i < obit.length; i++){
+  //   // constructor(c, s, col, spd, x, y, hX, hY){
+  //   let parameters = {
+  //     c: obit[i],
+  //     hX:
+  //   }
+  //   let letter = new Letter(parameters);
+  //   geneArray.push(letter);
+  // }
+  background(255);
+
+/*
+  let o = -1; //obit index
+  for (let x = 0; x < img.width; x++){
+    for (let y = 0; y <img.height; y++){ //reversed text vert
+      fill(greyArray[greyIndex]);
+      noStroke();
+      if (o < obit.length - 1){
+        o++;
+        text(obit[o], (width / img.width) * (x + 1), (height / img.height) * (y + 1));
+      } else {
+        o = 0;
+        text(obit[o], (width / img.width) * (x + 1), (height / img.height) * (y + 1));
+      }
+      greyIndex++;
+    }
+  }
+  */
+
+}
+
+function draw() {
+  background(255);
+  if (once) {
+    console.log(geneArray);
+
+    once = false;
+  }
+  //- - - - - - text movement test
+  for (let i = 0; i < geneArray.length; i ++){
+    geneArray[i].goHome();
+    geneArray[i].display();
+  }
+  
+  for (let i = 0; i < 100; i++){
+    console.log(geneArray[i].x + " X " + geneArray[i].homeX);
+    console.log(geneArray[i].y + " Y " + geneArray[i].homeY);
+
+  }
+
+}
