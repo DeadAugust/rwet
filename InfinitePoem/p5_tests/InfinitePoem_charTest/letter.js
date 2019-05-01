94f38e5bcc0e17af850f5165e837eacdae266552
DeadAugust <augustluhrs@gmail.com>
2019-05-01 15:19:09 -0400

    sharper portrait (lighter)

--- InfinitePoem/p5_tests/InfinitePoem_charTest/letter.js
+++ InfinitePoem/p5_tests/InfinitePoem_charTest/letter.js
@@ -1,6 +1,7 @@
-let defSize = 8;
+let defSize = 9;
 let defCol = "black";
 let defSpeed = 1;
+let defStep = 30;
 
 
 class Letter {
@@ -20,7 +21,7 @@ class Letter {
     // this.farX = abs(this.homeX - this.x);
     // this.farY = abs(this.homeY - this.y);
     // this.step = 50; //100 moves to home?
-    this.step = int(random(16, 24)) * int(random(2, 4));
+    this.step = int(random(defStep - (defStep/5), defStep + (defStep/5))) * int(random(2, 5)); //little more natural
     this.clock = this.step; //to count down
     //this.poem  //from python JSON
   }
@@ -41,42 +42,6 @@ class Letter {
       this.y = this.y + (this.farY / this.step);
       this.clock -= 1;
     }
-
-    //flicker issues (esp. with 0)
-    /*
-    if (this.x > this.homeX) {
-      this.x = int(this.x - (this.farX / this.step));
-    } else if (this.x < this.homeX) {
-      this.x = int(this.x + (this.farX / this.step));
-    }
-    if (this.y > this.homeY) {
-      this.y = int(this.y - (this.farY / this.step));
-    } else if (this.y < this.homeY) {
-      this.y = int(this.y + (this.farY / this.step));
-    }
-    */
-
-    //slightly correct, but way small
-    /*
-    let goX;
-    let goY;
-    if (this.x > this.homeX) {
-      goX = (this.x - this.homeX) / 2;
-    } else if (this.x < this.homeX) {
-      goX = (this.homeX - this.x) / 2;
-    } else {
-      goX = this.x;
-    }
-    if (this.y > this.homeY) {
-      goY = (this.y - this.homeY) / 2;
-    } else if (this.y < this.homeY) {
-      goY = (this.homeY - this.y) / 2;
-    } else {
-      goY = this.y;
-    }
-    this.x = goX;
-    this.y = goY;
-    */
   }
   interact() {
 
--- InfinitePoem/p5_tests/InfinitePoem_charTest/sketch.js
+++ InfinitePoem/p5_tests/InfinitePoem_charTest/sketch.js
@@ -18,8 +18,9 @@ function preload(){
 
 
 function setup() {
-  createCanvas(600, 816);
-  // frameRate(2);
+  // createCanvas(600, 816);
+  let w = (100/136) * windowHeight;
+  createCanvas(w, windowHeight);
   marg.loadPixels();
 
   obit = obit[0].split(" ");
@@ -35,6 +36,7 @@ function setup() {
     for (let y = 0; y <marg.height; y++){
       let index = ((y * marg.width) + x) * 4;
       let g = marg.pixels[index+2]; //greyscale, but y tho
+      g += 50; //just to sharpen a little? forgot up was lighter...
       if (o < obit.length - 1){
         o++;
       } else {
@@ -56,55 +58,21 @@ function setup() {
     }
   }
 
-
-  // for (let i = 0; i < obit.length; i++){
-  //   // constructor(c, s, col, spd, x, y, hX, hY){
-  //   let parameters = {
-  //     c: obit[i],
-  //     hX:
-  //   }
-  //   let letter = new Letter(parameters);
-  //   geneArray.push(letter);
-  // }
   background(255);
-
-/*
-  let o = -1; //obit index
-  for (let x = 0; x < img.width; x++){
-    for (let y = 0; y <img.height; y++){ //reversed text vert
-      fill(greyArray[greyIndex]);
-      noStroke();
-      if (o < obit.length - 1){
-        o++;
-        text(obit[o], (width / img.width) * (x + 1), (height / img.height) * (y + 1));
-      } else {
-        o = 0;
-        text(obit[o], (width / img.width) * (x + 1), (height / img.height) * (y + 1));
-      }
-      greyIndex++;
-    }
-  }
-  */
 
 }
 
 function draw() {
   background(255);
-  if (once) {
-    console.log(geneArray);
-
-    once = false;
-  }
+  // if (once) {
+  //   console.log(geneArray);
+  //
+  //   once = false;
+  // }
   //- - - - - - text movement test
   for (let i = 0; i < geneArray.length; i ++){
     geneArray[i].goHome();
     geneArray[i].display();
   }
-  // 
-  // for (let i = 0; i < 100; i++){
-  //   console.log(geneArray[i].x + " X " + geneArray[i].homeX);
-  //   console.log(geneArray[i].y + " Y " + geneArray[i].homeY);
-  //
-  // }
 
 }
